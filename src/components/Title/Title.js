37ace3dadb56f9b352f043d7ccc9f78a76a98709
Frank Valdez <fvaldez421@gmail.com>
2018-03-20 20:28:18 -0700

    added missing files

---
+++ src/components/Title/Title.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Title.css";
+
+const Title = props => (
+	<div className="header">
+		<h1 className="title">{props.children}</h1>
+		<h4>Click the images, do not repeat. </h4>
+		<h4>You have 3 chances.</h4>
+		<h3>Wins: {props.wins}</h3>
+		<h3>Losses: {props.losses}</h3>
+		<h2>{props.message}</h2>
+	</div>
+);
+
+export default Title;
